d92af6b8c630295019814cfff8c1687c6a1a2a5e
jabez137 <46041366+jabez137@users.noreply.github.com>
2018-12-21 16:30:42 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	include 'head.html';	
+?>
+<body>
+	<div class="wrap reset">
+		<?php
+			include 'header.html';	
+		?>
+		<div id="container">
+			<div class="page_name">정보입력</div>
+			<div id="main">
+				<div class="box_area">
+					<div class="sec sec1">
+						<div class="img_area"><img src="img/1_1.png" alt=""></div>
+					</div>
+					<div class="sec sec2">
+						<div class="img_area"><img src="img/1_2.png" alt=""></div>
+					</div>
+					<div class="sec sec3">
+						<div class="img_area"><img src="img/1_3.png" alt=""></div>
+					</div>
+					<div class="sec sec4 bottomP50">
+						<div class="img_area"><img src="img/1_4.png" alt=""></div>
+					</div>
+				</div>
+				<div class="btn_area block">
+<!-- 					<a href="" class="btn prev"><span class="icon arrow"></span>이전</a> -->
+					<a href="1.php" class="btn next">다음<span class="icon arrow"></span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
