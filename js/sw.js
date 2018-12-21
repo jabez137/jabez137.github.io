6002ff91704a1ea36cc708b4f81495d6927552cb
jabez137 <46041366+jabez137@users.noreply.github.com>
2018-12-21 17:33:43 +0900

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,18 +1,12 @@
-
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/css/style.css',
-  '/css/main.js'
-];
-
+console.log('Started', self);
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
 });
